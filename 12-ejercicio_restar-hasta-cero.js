5016cd07b1f995280d49872e77a10cb1af23c8ca
lenin <lnnsntg@gmail.com>
2022-04-14 21:04:43 +0200

    Ejercicio 12 restar hasta cero

---
+++ 12-ejercicio_restar-hasta-cero.js
@@ -0,0 +1,36 @@
+/* 
+Dado un número mostrar mostrar todos los numeros desde ese al cero en una
+lista con guiones donde cada fila debe empezar con nº
+*/
+
+function restarHastaCero(numero) {
+
+  salida = `--- Del ${numero}  al 0 --- \nnº --- ${numero} --- \n`;
+  while (numero >= 8) {
+    salida += `nº --- ${numero -= 8} --- \n`;
+  }
+  if (numero < 8) {
+    numero = 0;
+    salida += `nº --- ${numero} --- \n`;
+  }
+  salida += `--- FIN ---`;
+  return salida;
+}
+print = restarHastaCero(100);
+console.log(print);
+
+
+/* 
+function restarHastaCero(numero) {
+  let salida = "--- Del " + numero + " al 0 ---" + "\n";
+  while (numero > 0) {
+    salida += `-- nº ${numero} \n`
+    numero -= 8;
+  }
+  salida += `--- FIN ---`;
+  return salida;
+}
+
+print = restarHastaCero(100);
+console.log(print);
+*/
